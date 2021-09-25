52e9794aec4f10b6e750fff175cb608f4f394377
mtokovarov <62193412+mtokovarov@users.noreply.github.com>
2021-09-25 13:56:03 +0200

    Delete isolation_forest_definition directory

--- isolation_forest_definition/tree_grower_generalized_uniform.py
+++
@@ -1,9 +0,0 @@
-from tree_grower_generalized import TreeGrowerGeneralized
-import numpy as np
-
-class TreeGrowerGeneralizedUniform(TreeGrowerGeneralized):
-    def integratedProbabilityDensity(self, x):
-        return 0.5*x
-        
-    def probabilityDensity(self, x):
-        return 0.5
