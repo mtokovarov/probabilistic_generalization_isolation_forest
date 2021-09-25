b443ea94c2fee909f5ba7870ea3fe5de491c4923
mtokovarov <62193412+mtokovarov@users.noreply.github.com>
2021-09-25 13:56:44 +0200

    Add files via upload

---
+++ isolation_forest/tree_grower_generalized_uniform.py
@@ -0,0 +1,9 @@
+from isolation_forest.tree_grower_generalized import TreeGrowerGeneralized
+import numpy as np
+
+class TreeGrowerGeneralizedUniform(TreeGrowerGeneralized):
+    def integratedProbabilityDensity(self, x):
+        return 0.5*x
+        
+    def probabilityDensity(self, x):
+        return 0.5
